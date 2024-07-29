aa8d6999c62b8185f36a4d1f840916e4304e3979
Kiril Dudnik <kdudnik17@gmail.com>
2024-07-29 12:27:10 +0200

    fix: hero button router-link on full width

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -16,37 +16,35 @@ import NavBar from "../components/NavbarComponent.vue";
       <div
         class="flex flex-col text-lg mt-6 font-semibold leading-8 animate-fade-down animate-delay-150 text-gray-dark dark:text-white-dark"
       >
-        <span
-          v-for="(subtitle, index) in $tm('hero.subtitles')"
-          :key="index"
-        >
+        <span v-for="(subtitle, index) in $tm('hero.subtitles')" :key="index">
           {{ subtitle }}
         </span>
       </div>
-      <router-link to="/docs/rules">
-        <button class="group btn mt-16 flex gap-4 animate-fade-down animate-delay-300">
-          <span>{{ $t("hero.btn") }}</span>
-          <div
-            class="w-6 overflow-hidden flex justify-end duration-150 group-hover:w-9"
+      <router-link
+        to="/docs/rules"
+        class="group btn mt-16 flex gap-4 animate-fade-down animate-delay-300"
+      >
+        <span>{{ $t("hero.btn") }}</span>
+        <div
+          class="w-6 overflow-hidden flex justify-end duration-150 group-hover:w-9"
+        >
+          <svg
+            class="shrink-0"
+            width="38"
+            height="16"
+            viewBox="0 0 38 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              class="shrink-0"
-              width="38"
-              height="16"
-              viewBox="0 0 38 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                class="fill-white-dark dark:fill-gray-dark"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M31.3222 0L38 8L31.3222 16L29.7759 14.639L34.4448 9.04564H0V6.95436H34.4448L29.7759 1.36099L31.3222 0Z"
-                fill="#1BB9DC"
-              />
-            </svg>
-          </div>
-        </button>
+            <path
+              class="fill-white-dark dark:fill-gray-dark"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M31.3222 0L38 8L31.3222 16L29.7759 14.639L34.4448 9.04564H0V6.95436H34.4448L29.7759 1.36099L31.3222 0Z"
+              fill="#1BB9DC"
+            />
+          </svg>
+        </div>
       </router-link>
     </div>
     <FooterBar />
